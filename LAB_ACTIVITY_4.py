da7f23db790934f709da76cb4b491a55848dcb85
harryian1306 <harryianpasaoa3@gmail.com>
2025-03-11 22:11:29 +0800

    Add files via upload

---
+++ LAB_ACTIVITY_4.py
@@ -0,0 +1,8 @@
+check_number = int(input("What number are you thinking right now? I'm going to tell you if it is even or odd."))
+if check_number % 2 == 0:
+	print("It is an even number.")
+else:
+	print("It is an odd number.")
+
+
+	
